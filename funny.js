180f6986ba9245ec52eee76778436cb489915e1d
Ken Wilcox <wilcoxk@gmail.com>
2016-04-17 21:38:08 -0600

    Another coding challenge

---
+++ funny.js
@@ -0,0 +1,14 @@
+var that = {
+  is: {
+    a: {
+      test:'that.is.a.test'
+    },
+    not: {
+      a: {
+        test:'that.is.not.a.test'
+      },
+      funny: 'that.is.not.funny'
+    },
+    funny: 'that.is.funny'    
+  }
+}
