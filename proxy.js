14c8ad08e714d3baf515357764ecdb8665156a8c
Ken Wilcox <wilcoxk@gmail.com>
2016-04-20 11:23:02 -0600

    better example

--- proxy.js
+++ proxy.js
@@ -3,20 +3,14 @@
 
 var handler = {
     get: function(target, name) {               
-        if (target === this && target._message) {
-            target._message += "." + name.toString();
+        if (name == 'hello') {
+            return "world";
         } else {
-            target._message = name.toString();
+            return 42;
         }
-        return target;
     }
 };
 
-// Object.prototype.toString = function toMessageString() {
-//     debugger;
-//     if (this._message) return this._message;
-//     else return "dagnabbit";
-// };
-
-
 var that = new Proxy({}, handler);
+that.hello
+that.somethingElse
