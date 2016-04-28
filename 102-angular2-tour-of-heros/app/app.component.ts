aca5f6e62b332750d9b03e7771e74a8c3844407a
Ken Wilcox <wilcoxk@gmail.com>
2016-04-27 22:31:13 -0600

    you can select a hero now

--- 102-angular2-tour-of-heros/app/app.component.ts
+++ 102-angular2-tour-of-heros/app/app.component.ts
@@ -11,15 +11,17 @@ export class Hero {
       <h1>{{title}}</h1>
       <h2>My Heros</h2>
       <ul class="heros">
-        <li *ngFor="#hero of heros">
+        <li *ngFor="#hero of heros" (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>      
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+      <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>      
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
       </div>
       `,
     styles: [`
@@ -75,10 +77,8 @@ export class Hero {
 export class AppComponent {
     title = 'Tour of Heros';
     public heros = HEROS;
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
+    onSelect(hero: Hero) {this.selectedHero = hero;}
 }
 
 var HEROS: Hero[] = [
