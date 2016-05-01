456f363af6d557c4df18d955dce6ddabd80bbd16
Ken Wilcox <wilcoxk@gmail.com>
2016-04-30 22:40:33 -0600

    changed the order of imports

--- 102-angular2-tour-of-heros/app/app.component.ts
+++ 102-angular2-tour-of-heros/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
     selector: 'my-app',
