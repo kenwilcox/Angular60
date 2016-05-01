b212ea557a4e1831ff7581d6575eefe180439177
Ken Wilcox <wilcoxk@gmail.com>
2016-04-30 23:15:00 -0600

    More changes

--- 102-angular2-tour-of-heros/app/dashboard.component.ts
+++ 102-angular2-tour-of-heros/app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
 
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
@@ -11,14 +12,19 @@ export class DashboardComponent implements OnInit {
     
     heroes: Hero[] = [];
     
-    constructor(private _heroService: HeroService) { }
+    constructor(
+        private _router: Router,
+        private _heroService: HeroService) { 
+            
+        }
     
     ngOnInit() {
         this._heroService.getHeroesSlowly()
-        .then(heroes=> this.heroes = heroes.slice(1, 5));
+          .then(heroes=> this.heroes = heroes.slice(1, 5));
     }
     
-    gotoDetail() {
-        // not implemented yet
+    gotoDetail(hero: Hero) {
+        let link = ['HeroDetail', {id: hero.id}];
+        this._router.navigate(link);
     }
 }
